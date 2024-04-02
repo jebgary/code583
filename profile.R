bd4e0c43d8c5102f2c4909d83720d82a2d1b13ed
Jeb Gary <jgary4@vols.utk.edu>
2024-04-02 19:49:31 -0400

    new code R

--- profile.R
+++ profile.R
@@ -1,9 +1,14 @@
+library(data.table)
+library(stringr)
+
 Rprof()
 start <- Sys.time()
 
 columns_to_read = c("E Mail")
 datanew <- fread("Email Records.csv", select = columns_to_read)
 datanew <- datanew[1:10000,]
+
+
 datanew[, First_Name := str_extract(`E Mail`, "^[^.]+")]
 datanew[, Last_Name := str_extract(`E Mail`, "(?<=\\.)[^@]+")]
 datanew[, Domain := str_extract(`E Mail`, "(?<=@)[^.]+\\..+")]
@@ -11,3 +16,4 @@ datanew[, Domain := str_extract(`E Mail`, "(?<=@)[^.]+\\..+")]
 Rprof(NULL)
 summaryRprof()
 Sys.time() - start
+
